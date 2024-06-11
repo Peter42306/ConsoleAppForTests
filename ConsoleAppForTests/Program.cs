478fa729586fe486baf15155b69ff2df142a531f
Petr Zalizko <pzalizko@gmail.com>
2024-06-11 22:18:00 +0300

    Add project files.

---
+++ ConsoleAppForTests/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppForTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
